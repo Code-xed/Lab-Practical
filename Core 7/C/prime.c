e1a125add7cbc7339a8cc917abcc197446cfd9bf
Code-xed <ngudulu@gmail.com>
2023-04-15 13:49:14 +0530

    Update prime.c

--- Core 7/C/prime.c
+++ Core 7/C/prime.c
@@ -1,3 +1,29 @@
 #include <stdio.h>
+int prime(int n) {
+  int count = 1;
+  for (int i = 2; i <= n; i++) {
+    if (i % n == 0) {
+      count++;
+      }
+    }
+  if (count == 2) {
+    return 1;
+    }
+  else {
+    return 0;
+    }
+  }
 
+int main() {
+  int a;
+  printf("Enter A number to check if its a prime or not:/n");
+  scanf("%d", &a);
+  if (prime(a)) {
+    printf("Is prime");
+    }
+  else {
+    printf("Is not a Prime");
+    }
+  }
+  
 
